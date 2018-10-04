6a9e97762a05902f3823d69780b11495d68e8237
Robert Mihai Colca <robery_office@yahoo.ro>
2018-10-04 20:41:22 +0300

    Add files

---
+++ cmake-build-debug/CMakeFiles/training1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/training1.dir/main.cpp.o"
+  "training1.pdb"
+  "training1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/training1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define PI 3.14159265
+
+using namespace std;
+
+double calculateTurnRadius(double wheelBase, double steeringAngle) {
+    return wheelBase/sin(steeringAngle*PI/180);
+}
+
+double roundTwoDecimals(double number) {
+   return round(number*100)/100;
+}
+
+pair<double, pair<double, double>> calculateNewDirection(double wheelBase, double distance, double steeringAngle) {
+    double turnRadius = abs(calculateTurnRadius(wheelBase, steeringAngle));
+    double length = (2*PI) * turnRadius;
+
+    double computedNewDirection = distance*360 / length;
+
+    // we round it to 2 decimals
+    computedNewDirection = roundTwoDecimals(computedNewDirection);
+
+    double radianAngle =  computedNewDirection*PI/180;
+
+    double xCoordinate;
+
+    //if (steeringAngle > 0) {
+        xCoordinate = cos(radianAngle) * turnRadius;
+    //} else {
+    //    xCoordinate = turnRadius + cos(radianAngle) * turnRadius;
+    //}
+
+    while (computedNewDirection < 0) {
+        computedNewDirection += 360;
+    }
+
+    double yCoordinate = sin(radianAngle) * turnRadius;
+
+    // We round the coordinates to 2 decimals
+    xCoordinate = roundTwoDecimals(xCoordinate);
+    yCoordinate = roundTwoDecimals(yCoordinate);
+
+    return make_pair(computedNewDirection, make_pair(xCoordinate, yCoordinate));
+}
+
+int main() {
+    double x, y, z;
+
+    cin >> x >> y >> z;
+
+    auto computedNewDirectionResult = calculateNewDirection(x, y, z);
+
+    cout << computedNewDirectionResult.second.first << " " << computedNewDirectionResult.second.second << " " << computedNewDirectionResult.first << endl;
+
+    cout << endl << calculateTurnRadius(x, z);
+    return 0;
+}
